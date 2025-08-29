e0bdf6d1c3a2c1de6c5882f4b83d992e0965000c
Shadow737hub <nothinnothin162@gmail.com>
2025-08-29 16:15:40 +0530

    Create rarity.py

---
+++ EsproMusic/unit/rarity.py
@@ -0,0 +1,24 @@
+rarity_map = {
+    1: "Common 🟠",
+    2: "Legendary 🟡",
+    3: "Exclusive 💮",
+    4: "Limited 🔮",
+    5: "Celestial 🎐"
+}
+
+RARITY_NAMES = [
+    "Common 🟠",
+    "Legendary 🟡",
+    "Exclusive 💮",
+    "Limited 🔮",
+    "Celestial 🎐"
+]
+
+rarity_map2 = {
+    "Common 🟠": "🟠",
+    "Legendary 🟡": "🟡",
+    "Exclusive 💮": "💮",
+    "Limited 🔮": "🔮", 
+    "Celestial 🎐": "🎐"
+}
+
