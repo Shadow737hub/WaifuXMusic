637a6cffef556425741641f9f4fa163b13762039
Shadow737hub <nothinnothin162@gmail.com>
2025-08-29 16:06:17 +0530

    Update __init__.py

--- EsproMusic/__init__.py
+++ EsproMusic/__init__.py
@@ -1,21 +1,48 @@
+# ------------------------------ IMPORTS ---------------------------------
+import os
+from telegram.ext import Application
+from pyrogram import Client, filters as f
+from pyrogram.types import x
+
+application = Application.builder().token(TOKEN).build()
+
+x = x
+
+locks = {}
+message_counters = {}
+spam_counters = {}
+last_characters = {}
+sent_characters = {}
+first_correct_guesses = {}
+message_counts = {}
+last_user = {}
+warned_users = {}
+user_cooldowns = {}
+user_nguess_progress = {}
+user_guess_progress = {}
+normal_message_counts = {}
+
+# -------------------------- POWER SETUP ---------------------------------
+from EsproMusic.unit.zyro_ban import *
+from EsproMusic.unit.zyro_sudo import *
+from EsproMusic.unit.zyro_react import *
+from EsproMusic.unit.zyro_log import *
+from EsproMusic.unit.zyro_send_img import *
+from EsproMusic.unit.zyro_rarity import *
+
+# ----------------------- ESPRO MUSIC SETUP -------------------------------
 from EsproMusic.core.bot import Loy
 from EsproMusic.core.dir import dirr
 from EsproMusic.core.git import git
 from EsproMusic.core.userbot import Userbot
-from EsproMusic.misc import dbb, heroku
-
-from .logging import LOGGER
 
 dirr()
 git()
-dbb()
-heroku()
 
 app = Loy()
 userbot = Userbot()
 
-
-from .platforms import *
+from EsproMusic.core.platforms import *
 
 Apple = AppleAPI()
 Carbon = CarbonAPI()
@@ -24,3 +51,11 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
+
+# --------------------------- PLOG FUNCTION -------------------------------
+async def PLOG(text: str):
+    await app.send_message(
+        chat_id=GLOG,
+        text=text
+    )
+# ---------------------------- END OF CODE ------------------------------
