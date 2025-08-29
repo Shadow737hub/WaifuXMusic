709cdeaea3914182215c4bf426775b451724500c
Shadow737hub <nothinnothin162@gmail.com>
2025-08-30 00:14:33 +0530

    Update __init__.py

--- EsproMusic/__init__.py
+++ EsproMusic/__init__.py
@@ -1,30 +1,26 @@
-# EsproMusic/__init__.py
+from EsproMusic.core.bot import Loy
+from EsproMusic.core.dir import dirr
+from EsproMusic.core.git import git
+from EsproMusic.core.userbot import Userbot
+from EsproMusic.misc import dbb, heroku
 
-from pyrogram import filters as f
-from pyrogram.types import *
+from .logging import LOGGER
 
-# Export globals
-from EsproMusic.core.globals import (
-    locks, message_counters, spam_counters, last_characters,
-    sent_characters, first_correct_guesses, message_counts,
-    last_user, warned_users, user_cooldowns,
-    user_nguess_progress, user_guess_progress, normal_message_counts
-)
+dirr()
+git()
+dbb()
+heroku()
 
-# Export clients
-from EsproMusic.core.clients import (
-    app, userbot, Apple, Carbon, SoundCloud, Spotify,
-    Resso, Telegram, YouTube
-)
+app = Loy()
+userbot = Userbot()
 
-# Power setup
-from EsproMusic.unit.ban import *
-from EsproMusic.unit.sudo import *
-from EsproMusic.unit.react import *
-from EsproMusic.unit.send_img import *
-from EsproMusic.unit.rarity import *
 
-# Logging function
-async def PLOG(text: str):
-    from info import GLOG
-    await app.send_message(chat_id=GLOG, text=text)
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
